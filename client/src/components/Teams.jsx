e37e1c09147299d48f933a80761f51d0b79a539d
grantkalasky <grant.kalasky@berkeley.edu>
2019-05-07 11:52:41 -0700

    finished team page

--- client/src/components/Teams.jsx
+++ client/src/components/Teams.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 // import Client from "../Client.js";
+import '../styles/Teams.css';
 
 export default class Teams extends React.Component {
   _isMounted = false;
@@ -51,9 +52,12 @@ export default class Teams extends React.Component {
   }
 
   render() {
+    const { teamResults } = this.state;
+
     return(
       <div className="teams-container">
         <h2>Welcome to the Teams page</h2>
+        <p>Search for your favorite soccer teams and view their attributes for different matches.</p>
         <div className="ui icon input">
           <input 
             type="text" 
@@ -64,6 +68,46 @@ export default class Teams extends React.Component {
           />
           <i className="search icon"></i>
         </div>
+        <div className="team-card-container">
+          {teamResults.length > 0 && 
+            <div className="ui cards">
+              {teamResults.map(team => (
+                <div className="ui card">
+                  <div className="content">
+                    <div className="header">
+                      {team.team_long_name}
+                    </div>
+                    <div className="description">
+                      {team.team_short_name} - {team.date.slice(0,9)}
+                    </div>
+                  </div>
+                  <div className="content">
+                    <span className="right floated">
+                      {team.buildUpPlaySpeedClass}
+                    </span>
+                    <i class="bolt icon"></i>
+                    Speed: {team.buildUpPlaySpeed}
+                  </div>
+                  <div className="content">
+                    <span className="right floated">
+                      {team.buildUpPlayDribblingClass}
+                    </span>
+                    <i class="futbol outline icon"></i>
+                    Dribbling: {team.buildUpPlayDribbling}
+                  </div>
+                  <div className="content">
+                    <span className="right floated">
+                      {team.buildUpPlayPassingClass}
+                    </span>
+                    <i class="male icon"></i>
+                    Passing: {team.buildUpPlayPassing}
+                  </div>
+                </div>
+              ))}
+            </div>
+          }
+        </div>
+        
       </div>
     );
   }
