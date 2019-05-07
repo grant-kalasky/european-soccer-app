3949230bc597e9adef15367c5f30fb3ac6001335
grantkalasky <grant.kalasky@berkeley.edu>
2019-05-06 18:39:13 -0700

    added semantic ui

--- client/src/components/Teams.jsx
+++ client/src/components/Teams.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import '../semantic/dist/semantic.min.css';
 
 const Teams = () => (
   <div className="teams-container">
     <h2>Welcome to the Teams page</h2>
+    <div className="ui icon input">
+      <input type="text" placeholder="Search for team..." />
+      <i className="search icon"></i>
+    </div>
   </div>
 );
 
